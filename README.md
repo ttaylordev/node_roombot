5cc09c7f566051007291de8abe3c51aa441d4ea3
Tyson <mellowt@gmail.com>
2016-07-03 18:56:42 -0600

    .

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Browswer Roombot
 
 A starter kit for driving a roombot.
+http://roombots.mx.com/simulations
 
 ## Driving The Simulator
 
@@ -20,3 +21,122 @@ Edit your `app.js` file and set `roombotIP = '<IP-of-the-roombot>';`.
 Now when you run the application you should be connected to the real roombot.
 
 Good luck on your driving adventures! :tada: :+1:
+
+
+
+## JSON Structure
+All the JSON messages you send to the websocket and receive from the websocket will have a similar structure. The structure is the standard followed by Phoenix Channels.
+
+topic All messages happen in a topic. Roombot always uses "topic":"roomba".
+event The event is a sort of type for a message. It makes it easy to distinguish between a move command an an update about the sensors on the roomba.
+ref A ref is a unique identifier for a given message. Each time you send a message to the roombot, the roombot can respond that request and it will include the same ref.
+payload This is where you put any data that are sending and where the roombot puts any data that is is sending. It is always a JSON object (never a list or a single scalar value).
+
+
+## An Example Session
+The best way to describe these messages is just to give an example of what your program might send and receive. I'll put some notes along the way to help point out the patterns you can depend on.
+
+
+
+When you first connect to the roombot you will need to send a phx_join message. This tells the roombot that it should start sending you status updates and expect you to send driving commands.
+Your Program => Roombot
+
+```javascript
+
+{
+  "topic":"roomba", // All messages happen in a topic. Roombot always uses "topic":"roomba".
+  "event":"phx_join", // The event is a sort of type for a message. It makes it easy to distinguish between a move command and an update about the sensors on the roomba.
+  "ref":1, // A ref is a unique identifier for a given message. Each time you send a message to the roombot, the roombot can respond that request and it will include the same ref.
+  "payload":{} // This is where you put any data that are sending and where the roombot puts any data that is is sending. It is always a JSON object (never a list or a single scalar value).
+}
+
+```
+
+The Roombot will send back a phx_reply message to let you know that you have successfully joined the topic.
+
+Note: the roombot sent back "ref": 1 because that is the ref we sent when we asked to join the topic. The "ref" is how we know which messages from the roombot are responses to our requests.
+Roombot => Your Program
+
+```javascript
+{
+  "topic":"roomba",
+  "event":"phx_reply",
+  "ref":1,
+  "payload":{
+    "status":"ok",
+    "response":{}
+  }
+}
+```
+
+Now the roombot will start to send you sensor updates. These messages tell you what is happening with the roomba.
+
+light_bumper_* these are infrared sensors along the front of the roomba that detect objects between 1-3" away. They have a value of 0 or 1.
+bumper_* the physical bumper sensors. Also have values of 0 or 1.
+battery_* the capacity and current charge of the battery measured in mAh
+For more details please see sensors page.
+Roombot => Your Program
+
+```javascript
+{
+  "topic":"roomba",
+  "ref":null,
+  "payload":{
+    "light_bumper_right_front":0,
+    "light_bumper_right_center":0,
+    "light_bumper_right":0,
+    "light_bumper_left_front":0,
+    "light_bumper_left_center":1,
+    "light_bumper_left":0,
+    "bumper_right":0,
+    "bumper_left":0,
+    "battery_charge":2751,
+    "battery_capacity":2366
+  },
+  "event":"sensor_update"
+}
+```
+
+Once you know where you want to go you can send a drive command. The velocity is measured in mm/s. -500 to 500 are the valid values. The radius is a desired radius you want to turn through. It is measures as mm between the center of the roomba and the center of the turning circle. Negative values turn left and positive values turn right. -2000 to 2000 are valid values. 0 will drive straight.
+Your Program => Roombot
+
+```javascript
+{
+  "topic":"roomba",
+  "event":"drive",
+  "ref":12,
+  "payload":{
+    "velocity": 100,
+    "radius": -500
+  }
+}
+```
+
+Sometimes you will want to healthcheck your websocket or just send a small message to keep the network connection active. This will keep the latency low between the roombot and your program. To do this you can send a heartbeat message
+Your Program => Roombot
+
+```javascript
+{
+  "topic":"phoenix",
+  "event":"heartbeat",
+  "payload":{},
+  "ref":"6"
+}
+```
+
+The Roombot will send back a phx_reply message to let you know that it got your heartbeat.
+
+Note: I send a hearbeat once per second for most of my driving programs.
+Roombot => Your Program
+
+```javascript
+{
+  "topic":"phoenix",
+  "ref":"6",
+  "payload":{
+    "status":"ok",
+    "response":{}
+  },
+  "event":"phx_reply"
+}
+```
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const WebSocketClient = require('websocket').client;
+const WebSocketClient = require( 'websocket' ).client;
 const client = new WebSocketClient();
 
-const channel = 'simulation:wmYNlQ==';
 const roombaIP = 'roombots.mx.com';
+const channel = 'simulation:jwnXRA==';
+const socketConnection = 'ws://roombots.mx.com:80/socket/websocket';
 
-const phxJoin = function (connection) {
+
+const slow = 300;
+const medium = 1000;
+const fast = 2000;
+const veryFast = 5000;
+
+const straight = 0;
+const radiusTight = 100;
+const radiusMedium = 700;
+const radiusWide = 1000;
+
+
+
+
+var vel = 8000;
+var rad = 0;
+
+
+
+const phxJoin = function ( connection ) {
   const initMessage = {
     topic: channel,
     event: 'phx_join',
@@ -12,13 +32,13 @@ const phxJoin = function (connection) {
     payload: {}
   };
 
-  if (connection.connected) {
-    connection.sendUTF(JSON.stringify(initMessage));
+  if ( connection.connected ) {
+    connection.sendUTF( JSON.stringify( initMessage ) );
   }
 };
 
-const heartbeat = function (connection) {
-  console.log('Heart Beating');
+const heartbeat = function ( connection ) {
+  console.log( 'Heart Beating' );
 
   const heartbeatMessage = {
     topic: 'phoenix',
@@ -27,42 +47,374 @@ const heartbeat = function (connection) {
     ref: 10
   };
 
-  setInterval(function () {
-    connection.sendUTF(JSON.stringify(heartbeatMessage));
-  }, 1000)
+  setInterval( function () {
+    console.log( 'pulse-thump' );
+    connection.sendUTF( JSON.stringify( heartbeatMessage ) );
+  }, 1000 )
 };
 
-const drive = function (connection) {
-  connection.on('message', function (message) {
-    const response = JSON.parse(message.utf8Data);
+////////  DRIVE  \\\\\\\\
+const drive = function ( connection ) {
+  connection.on( 'message', function ( message ) {
+    const response = JSON.parse( message.utf8Data );
+    // console.log(connection);
+    // console.log( response );
 
-    if (response.event === 'phx_reply' && response.ref === 1) {
-      const driveMessage = {
-        topic: channel,
-        event: 'drive',
-        ref: 15,
-        payload: {
-          velocity: 100,
-          radius: 50
-        }
-      };
+    ///////  ARRAY APPROACH  \\\\\\\
 
-      connection.sendUTF(JSON.stringify(driveMessage));
+    var load = response.payload
+    var arr = [ load.light_bumper_left, load.light_bumper_left_center, load.light_bumper_left_front, load.light_bumper_right, load.light_bumper_right_center, load.light_bumper_right_front ]
+
+    var centerArr = [ load.light_bumper_left_front, load.light_bumper_right_front ]
+    var leftArr = [ load.light_bumper_left, load.light_bumper_left_center, load.light_bumper_left_front ]
+    var rightArr = [ load.light_bumper_right, load.light_bumper_right_center, load.light_bumper_right_front ]
+
+    var sum = arr.reduce( ( a, b ) => a + b, 0 );
+
+
+    var actionDone;
+    // function whatever() {
+    //   if ( !actionDone ) {
+    //     actionDone = true;
+    //   }
+    // }
+    // console.log(response.payload);
+
+    // minor adjustment interval function
+    // large correction(right or left)
+    //
+    /*
+            ARRAY     Action Needed           Condition       Refined Condition                 Refined further Bi-Dec#
+            1 0 0 0 0 0 -minor adjustment   --if 1 0 0 0 0 0  if(arr[0] && !arr[1,2,3,4,5])       if(arr[0] && !sum)                  --32
+            1 0 0 0 0 0 -minor adjustment   --if 0 0 0 0 0 1  if(arr[5] && !arr[0,1,2,3,4])       if(arr[5] && !sum)                  --1
+            1 1 0 0 0 0 -medium adjustment  --if 1 1 x x x 0  if(arr[0] && arr[1])                if(!arr[5] && sum >= 2)             --62-48
+            0 0 0 0 1 1 -medium adjustment  --if 0 x x x 1 1  if(arr[4] && arr[5])                if(!arr[0] && sum >= 2)             --3-31
+            0 1 1 1 1 0 -large correction   --if 0 1 x x 1 0  if(arr[1] && arr[4] && !arr[0,5])   if(!arr[0] && !arr[5] && sum >= 2)  --18-30
+            0 0 1 1 0 0 -large correction   --if 0 x 1 1 x 0  if(arr[2] && arr[3] && !arr[0,5])    ^^       ^^         ^^             --12-30
+            1 1 1 1 1 1 -backup large corr                                                        if(sum === 6)
+                                                                                                  if(sum > 3)
+          //  else resume(fast);
+        */
+    /// logging sensor output \\
+    if ( sum > 0 ) {
+      console.log( 'arr: ' + arr + ', sum: ' + sum );
     }
-  });
+
+
+// cases \\
+    if ( arr[ 0 ] && !sum ) {
+      console.log( 'small, right' );
+      minorAdj( 'right' );
+    }
+
+    if ( arr[ 5 ] && !sum ) {
+      console.log( 'small, left' );
+      minorAdj( 'left' );
+    }
+
+    if ( !arr[ 5 ] && sum >= 2 ) { //
+      console.log( 'medium, right' );
+      mediumAdj( 'right' );
+    }
+
+    if ( !arr[ 0 ] && sum >= 2 ) { //
+      console.log( 'medium, left' );
+      mediumAdj( 'left' );
+    }
+
+    if ( ( !arr[ 0 ] && !arr[ 5 ] && sum >= 2 ) || ( sum === 6 ) ) {
+      console.log( 'large, right' );
+      largeCorr( 'right' );
+    } else
+    /// PROCEED \\\
+    function proceed( speed ) {
+      speed = speed || fast
+      vel = speed;
+      rad = 0;
+    };
+
+
+
+
+    ////   massive potential   \\\\\\\\
+    ///  could be used to convert text to image, if the bot was small enough \\\
+    /// could make sketches on cavas from photos. it could be programmed to collide only with certain colors \\\
+
+    // write a function to determine vector based on speed, radius, and duration(time) of turn
+    // use a combination of speed and degrees to plot x, y coordinates
+    // log the coordinates, on new position(not already cached)
+    // draw a map of the area
+// TODO: backup on medium adjustments
+
+    /// MINOR adjustment \\\
+    function minorAdj( direction, speed ) {
+      speed = speed || fast
+      console.log( 'Minor adjustment made to the: ' + direction );
+      vel = speed;
+      if ( direction == 'right' ) {
+        rad = radiusWide
+      } else
+        rad = -( radiusWide )
+    }
+
+    /// MEDIUM adjustment \\\
+    function mediumAdj( direction, speed ) {
+      speed = speed || medium
+      console.log( 'Medium adjustment made to the: ' + direction );
+      vel = speed;
+      if ( toString(direction) == 'right' ) {
+        rad = radiusMedium
+      } else {
+        rad = -( radiusMedium )
+      }
+    };
+
+    /// LARGE correction \\\
+    function largeCorr( direction, speed ) {
+      speed = speed || slow
+      console.log( 'Large correction made to the: ' + direction );
+      vel = speed;
+      if ( direction == 'right' ) {
+        rad = radiusTight
+      } else {
+        rad = -( radiusTight )
+      }
+    };
+
+    ////// DRIVE MESSAGE \\\\\\\
+    const driveMessage = {
+      topic: channel,
+      event: 'drive',
+      ref: 15,
+      payload: {
+        velocity: vel,
+        radius: rad
+      }
+    }
+    connection.sendUTF( JSON.stringify( driveMessage ) );
+
+  } );
 }
 
-client.on('connectFailed', function (error) {
-  console.log('Connect Error: ' + error.toString());
-});
 
-client.on('connect', function (connection) {
-  console.log('WebSocket Client connected');
 
-  phxJoin(connection);
-  heartbeat(connection);
-  drive(connection);
-});
+//  WATCHING
 
-client.connect('ws://' + roombaIP + '/socket/websocket?vsn=1.0.0');
 
+client.on( 'sensor_update', function ( message ) {
+  const reponse = JSON.parse( message.utf8Data );
+  // console.log( response );
+
+} )
+client.on( 'connectFailed', function ( error ) {
+  console.log( 'Connect Error: ' + error.toString() );
+} );
+
+client.on( 'connect', function ( connection ) {
+  console.log( 'WebSocket Client connected' );
+
+  phxJoin( connection );
+  heartbeat( connection );
+  drive( connection );
+} );
+
+// client.connect('ws://' + roombaIP + '/socket/websocket?vsn=1.0.0');
+client.connect( socketConnection );
+
+
+
+/*
+// not sure if my cases can be expressions. \\
+    switch ( arr ) {
+    case ( arr[ 0 ] && !sum ):
+      console.log( 'hello' );
+      minorAdj( 'right' );
+      break;
+    case ( arr[ 5 ] && !sum ):
+      console.log( 'hello' );
+      minorAdj( 'left' );
+      break;
+    case ( !arr[ 5 ] && sum >= 2 ):
+      console.log( 'hello' );
+      mediumAdj( 'right' );
+      break;
+    case ( !arr[ 0 ] && sum >= 2 ):
+      console.log( 'hello' );
+      mediumAdj( 'left' );
+      break;
+    case ( ( !arr[ 0 ] && !arr[ 5 ] && sum >= 2 ) || ( sum > 4 ) ):
+      console.log( 'hello' );
+      largeCorr();
+      break;
+    default:
+      proceed();
+
+    }
+*/
+
+
+
+
+
+/*
+/////// frontal collision \\\\\\\\
+if ( response.payload.light_bumper_left_center > 0 || response.payload.light_bumper_right_center > 0 ) {
+  console.log( response.payload );
+  if ( !actionDone ) {
+    console.log( 'executing frontal collision ' );
+    console.log( ' actionDone is equal to:' + actionDone );
+    vel = -2000;
+    rad = 0; /* -132 */
+/*
+    var newAction;
+    actionDone = true;
+  }
+  setTimeout( function () {
+    if ( !newAction ) {
+      console.log( actionDone );
+      console.log( 'resuming front frontal collision, after backing up' );
+      vel = 4000;
+      rad = 0;
+      actionDone = false;
+    }
+  }, 2000 );
+}
+*/
+
+
+
+
+/**/
+
+
+/// listen for sensor data
+// if sensor data
+/// handle sensor data
+/// make correction
+// give a vel & rad,
+/// proceed
+
+////// RIGHT  IR \\\\\\\
+//  Far RIGHT
+// if ( response.payload.light_bumper_right > 0 ) {
+//   console.log( response.payload + 'from farRight' );;
+//   vel = -200;
+//   rad = -70;
+//   setTimeout( function () {
+//     console.log( response.payload + 'resuming from farRight' );
+//     vel = 4000;
+//     rad = 0;
+//   }, 1000 );
+// }
+//
+//
+// //  right CENTER
+// if ( response.payload.light_bumper_right_center > 0 ) {
+//   console.log( response.payload );
+//   vel = -200;
+//   rad = -132;
+//   setTimeout( function () {
+//     vel = 4000;
+//     rad = 0;
+//   }, 1000 );
+//       // }
+
+// //  right FRONT
+// if ( response.payload.light_bumper_right > 0 ) {
+//   console.log( response.payload );
+//   vel = -200;
+//   rad = -100;
+//   setTimeout( function () {
+//     vel = 4000;
+//     rad = 0;
+//   }, 1000 );
+//       // }
+
+
+
+
+
+////// LEFT  IR \\\\\\\
+
+// Far left
+// if ( response.payload.light_bumper_left > 0 ) {
+//   console.log( response.payload +" = load" );
+//   console.log('turning from Far Left');
+//   console.log(arr);
+//
+//   // set boolean to true closure
+//   var bool = true;
+//   vel = 200;
+//   rad = -70;
+//   setTimeout( function () {
+//     console.log('resuming from Far left');
+//     bool = false;
+//     vel = 1000;
+//     rad = 0;
+//     console.log('after interval' + arr);
+//   }, 700 );
+// }
+
+
+//  Left center
+// if ( response.payload.light_bumper_left_center > 0 ) {
+//   console.log( response.payload );
+//   vel = -200;
+//   rad = -132;
+//   setTimeout( function () {
+//     vel = 4000;
+//     rad = 0;
+//   }, 1000 );
+//
+// }
+
+// //  Left front
+// if ( response.payload.light_bumper_left > 0 ) {
+//   console.log( response.payload );
+//   vel = -200;
+//   rad = -100;
+//   setTimeout( function () {
+//     vel = 4000;
+//     rad = 0;
+//   }, 1000 );
+//
+// }
+
+///////  BUMPERS  \\\\\\\\\
+/*
+    // bumper_left
+    if ( response.payload.bumper_left > 0) {
+      console.log( response.payload );
+      vel = -2000;
+      rad = -133;
+      setTimeout( function () {
+        vel = 4000;
+        rad = 0;
+      }, 1000 );
+    }
+
+    // bumper_right
+    if ( response.payload.bumper_right > 0) {
+      console.log( response.payload );
+      vel = -2000;
+      rad = 133;
+      setTimeout( function () {
+        vel = 4000;
+        rad = 0;
+      }, 1000 );
+    }
+
+*/
+
+
+
+
+
+
+
+
+// if statements for obstacles
+// acces sensor data
+
+// function on.update do X
+// Brian used a count with small increments, the count is higher depending on the severity
